00c6c72ed6511261618e2b20d21e04f0c5139dfa
ze-fernando <zefernando.dev@gmail.com>
2025-07-21 13:43:35 -0300

    feat: request payload

--- src/routes/ai_route.py
+++ src/routes/ai_route.py
@@ -5,3 +5,11 @@ route = APIRouter()
 @route.get('/')
 def main():
     return {'message': 'hello world'}
+
+
+@route.post('/')
+def make_timeline(plan: StudyPlanRequest):
+    return {
+        'subjects': plan.subjects,
+        'days': plan.availability
+    }
